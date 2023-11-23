3e47558d4e9d85a0f19cb5d866108d0c2cfa5d13
ddz2456820347 <2456820347@qq.com>
2023-11-23 12:26:00 +0800

    update

--- app/components/whyPetsnowy/index.tsx
+++ app/components/whyPetsnowy/index.tsx
@@ -1,4 +1,4 @@
-import styles from '~/styles/whyPetsnowy/index.css';
+import styles from '../../styles/whyPetsnowy/index.css';
 import {LinksFunction} from '@shopify/remix-oxygen';
 
 export const links: LinksFunction = () => [{rel: 'stylesheet', href: styles}];
