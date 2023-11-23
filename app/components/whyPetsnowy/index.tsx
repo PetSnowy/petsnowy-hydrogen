001b2e9c8a255ff42366dd1cd06af26f7a5870dc
ddz2456820347 <2456820347@qq.com>
2023-11-23 12:30:58 +0800

    update

--- app/components/whyPetsnowy/index.tsx
+++ app/components/whyPetsnowy/index.tsx
@@ -1,8 +1,8 @@
-import styles from '../../styles/whyPetsnowy/index.css';
+import styles from '~/styles/whyPetsnowy/index.css';
 import {LinksFunction} from '@shopify/remix-oxygen';
 
 export const links: LinksFunction = () => [{rel: 'stylesheet', href: styles}];
 
 export default function index() {
-  return <div className="why-petsnowy">index</div>;
+  return <div className='why-petsnowy'>index</div>;
 }
